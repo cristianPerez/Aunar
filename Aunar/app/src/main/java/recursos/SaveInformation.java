d69681da503d687a03c41d6abee9ba7a0d558fc4
cristian camilo perez cardona <cperez354@gmail.com>
2014-10-04 15:40:13 -0500

    first commit
    first commit

---
+++ Aunar/app/src/main/java/recursos/SaveInformation.java
@@ -0,0 +1,67 @@
+package recursos;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
+import android.net.ConnectivityManager;
+import android.os.AsyncTask;
+import org.json.JSONArray;
+import org.json.JSONException;
+
+public class SaveInformation extends AsyncTask<String, Void, Void> {
+	
+	private WebService connection;
+	private Activity activity;
+	private SharedPreferences sharedpreferences;
+	
+	public SaveInformation(Activity activity){
+		this.connection = new WebService();
+		this.activity = activity;
+		this.sharedpreferences = activity.getSharedPreferences("MyPreferences", Context.MODE_PRIVATE);
+	}
+
+	@Override
+	protected void onPreExecute() {
+	}
+
+	@Override
+	protected Void doInBackground(String... params){
+		JSONArray answer;
+		String token = this.sharedpreferences.getString("TOKEN", null);
+		if(this.thereIsInternet()){
+				this.connection.setUrl("http://www.concesionesdeaseo.com/pruebas/FUNEventosMovil/Eventos");
+				String[] parameters = {"10",token,"","backup",""};
+				answer = this.connection.conectar(parameters);
+				try {
+					if(answer.getJSONObject(0).getString("mensaje").equals("1")){
+
+
+					}
+				} catch (JSONException e) {
+					e.printStackTrace();
+				}
+		}
+		else{
+			//Toast
+		}
+		return null;
+	}
+
+	@Override
+	protected void onPostExecute(Void result) {
+		
+	}
+	
+	private boolean thereIsInternet() {
+		ConnectivityManager cm = (ConnectivityManager) activity.getSystemService(Context.CONNECTIVITY_SERVICE);
+		if (cm.getActiveNetworkInfo() != null
+				&& cm.getActiveNetworkInfo().isAvailable()
+				&& cm.getActiveNetworkInfo().isConnected()) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+}
